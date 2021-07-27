48ba1509060712a15375f46637126e346e28ae88
LuizTFreitas <freitasow@gmail.com>
2021-07-27 15:29:03 -0300

    Estrutura Inicial com exemplos finalizados

--- src/main/java/com/luizfreitas/springblog/controller/BlogController.java
+++ src/main/java/com/luizfreitas/springblog/controller/BlogController.java
@@ -4,11 +4,15 @@ import com.luizfreitas.springblog.model.Post;
 import com.luizfreitas.springblog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -30,5 +34,19 @@ public class BlogController {
         Post post = blogService.finById(id);
         mv.addObject("post",post);
         return mv;
+    }
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm(){
+        return "postForm";
+    }
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("message","Verifique se os campos obrigatórios foram preenchidos!");
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        blogService.save(post);
+        return "redirect:/posts";
     }
 }
